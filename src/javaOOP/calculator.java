786e038428d4a01956673ca37ec619ed3fb8b6af
Joshua Lopez <jrenelopez92@gmail.com>
2021-05-16 23:50:31 -0500

    created a method from another

---
+++ src/javaOOP/calculator.java
@@ -0,0 +1,14 @@
+package javaOOP;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		}
+		public static int calc(int num1, int num2) {
+			return num1 + num2; 
+		}
+		
+
+	
+
+}
---
+++ src/javaOOP/runner.java
@@ -0,0 +1,17 @@
+package javaOOP;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	int result = calculator.calc(5 , 5);
+	
+	
+		System.out.println(result);
+	}
+	
+}
+
+	
